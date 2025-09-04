6477c8810e0aac8a18894c787ced6064651d65e5
parkboseung <endeveloper96@gmail.com>
2025-09-04 20:41:35 +0900

    테스트 코드 작성 및 jvmArgs '-Xshare:off' // JVM 아규먼트 설정 추가

--- src/main/java/com/group/pre_side_shoppingMall/dto/user/request/UserLoginRequest.java
+++ src/main/java/com/group/pre_side_shoppingMall/dto/user/request/UserLoginRequest.java
@@ -1,8 +1,12 @@
 package com.group.pre_side_shoppingMall.dto.user.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginRequest {
 
     private String userName;
